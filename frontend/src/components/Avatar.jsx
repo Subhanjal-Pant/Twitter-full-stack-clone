5bcf664d83b9436289e1adba95939c93a4a5ca9e
Subhanjal-Pant <subhanjalpant1@gmail.com>
2025-05-24 19:23:38 +0545

    Bug solved Problem with react-avatar packagae

---
+++ frontend/src/components/Avatar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Avatar = () => {
+  return (
+    <div className='border-[100%] bg-blue-400'>
+        <img src="" alt="" />
+    </div>
+  )
+}
+
+export default Avatar
---
+++ frontend/src/components/CreatePostTwo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CreatePostTwo = () => {
+  return (
+    <div>
+        <Avatar />
+        <input type="text" />
+    </div>
+  )
+}
+
+export default CreatePostTwo
